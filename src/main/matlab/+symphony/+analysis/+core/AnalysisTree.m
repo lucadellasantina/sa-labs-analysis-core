607be782852b1575feadb8bbab4f7797555527f2
Sathish Kumar Narayanan <ragav.sathish@gmail.com>
2016-07-18 23:50:38 +0300

    completed refactoring analysis tree and builder

--- src/main/matlab/+symphony/+analysis/+core/AnalysisTree.m
+++ src/main/matlab/+symphony/+analysis/+core/AnalysisTree.m
@@ -1,279 +1,62 @@
 classdef AnalysisTree < tree
     
     properties
-        filename    % h5 file name    
+        filename    % h5 file name
         filepath    % h5 file path
         name        % Descriptive name of analysis
     end
     
     methods
         
-        function obj = AnalysisTree()
-            %constructor, empty for now
-        end
-        
-        function obj = setName(obj, name)
-            nodeData = struct;
-            nodeData.name = name;
-            obj = obj.set(1, nodeData);
-        end
-        
-        function obj = copyAnalysisParams(obj, params)
-           nodeData = obj.get(1);
-           names = fieldnames(params);           
-           for i=1:length(names)
-              nodeData.(names{i}) = params.(names{i});
-              if isprop(obj, names{i})
-                  obj.(names{i}) = params.(names{i});
-              end
-           end
-           obj = obj.set(1, nodeData);
-        end
-        
-        function obj = addTreeLevel(obj, oldTree, analysisType, param)
-            if isempty(param)
-                return;
-            end
-            
-            chInd = oldTree.getchildren(1);            
-            L = length(chInd);
-            values = cell(1,L);
-            for i=1:L
-                %find nodes that are of the right class and have a value for this parameter 
-                %set other values to 'not part of analysis'
-                curNode = oldTree.get(chInd(i));
-                if strcmp(curNode.class, analysisType) && isfield(curNode, param)
-                   values{i} = num2str(curNode.(param));                                   
-                else
-                   values{i} = 'not part of analysis';
-                end
-            end       
-            
-            uniqueVals = unique(values);
-            uniqueVals = setdiff(uniqueVals, 'not part of analysis');
-            Nvals = length(uniqueVals);
-            for n=1:Nvals
-               newNode = struct;
-               newNode.name = [param '=' uniqueVals{n}];
-               newNode.(param) = uniqueVals{n};
-               obj = obj.addnode(1, newNode);
-               newTree_chInd = obj.getchildren(1);
-
-               for i=1:L                  
-                  if strcmp(values{i}, uniqueVals{n}) %if matching value    
-                    %disp('found match');
-                    obj = obj.graft(newTree_chInd(n), oldTree.subtree(chInd(i)));
-                  end
-               end               
-            end
-        end
-        
-        function obj = buildCellTree(obj, rootNodeID, cellData, dataSet, paramList)
-            if isempty(paramList)
-                return;
-            end
-            
-            if isa(paramList{1}, 'function_handle')
-                fun = paramList{1};
-                values = arrayfun(@(d) execute(fun, cellData.epochs(d)), dataSet);
-            else
-                values = cellData.getEpochVals(paramList{1}, dataSet);
-            end
-            
-            parameter = paramList{1};
-            uniqueValues = unique(values);
-            for i = 1 : length(uniqueValues)
-                value = get(uniqueValues, i);
-                newDataSet = filter(dataSet, values, value);
-
-                if ~ isempty(newDataSet)
-                    nodeData = struct();
-                    
-                    if isa(parameter, 'function_handle')
-                        nodeData.splitParam = func2str(parameter);
-                        nodeData.name = [func2str(parameter) '==' num2str(value)];
-                    else
-                        nodeData.splitParam = parameter;
-                        nodeData.name = [parameter '==' num2str(value)];
-                    end
-                    
-                    nodeData.splitValue = value;
-                    nodeData.epochID = newDataSet;
-                    [obj, newID] = obj.addnode(rootNodeID, nodeData);
-                    
-                    %recursive call
-                    if length(paramList) > 1
-                        obj = obj.buildCellTree(newID, cellData, newDataSet, paramList(2 : end));
-                    end
-                end
-            end
-
-            function ds = filter(dataSet, values, key)
-                ds = [];
-                for j = 1 : length(dataSet)
-                    destKey = get(values, j);
-                    
-                    if ischar(key)
-                        if strcmp(key, destKey)
-                            ds = [ds dataSet(j)];
-                        end
-                    else
-                        if key == destKey
-                            ds = [ds dataSet(j)];
-                        end
-                    end
-                end
-            end
-
-            function value = get(object, index)
-                if iscell(object)
-                   value = object{index};
-                else
-                   value = object(index);    
-                end
-            end
-
-            function result = execute(fun, args)
-
-               result = fun(args);
-                if ~ isscalar(result)
-                    result = num2str(result);
-                end
-            end
-        end
-        
-        function obj = copyParamsFromSampleEpoch(obj, cellData, dataSet, params)
-            sampleEpoch = cellData.epochs(dataSet(1));
-            nodeData = obj.get(1);
-            for i=1:length(params)
-                nodeData.(params{i}) = sampleEpoch.get(params{i});
-            end
-            obj = obj.set(1, nodeData);
-        end
-        
         function cellName = getCellName(obj, nodeInd)
-            nodeData = obj.get(nodeInd);
-            cellName = '';
-            while ~isfield(nodeData, 'cellName');
-                nodeInd = obj.getparent(nodeInd);
-                if nodeInd == 0
-                   return;
-                end
-                nodeData = obj.get(nodeInd);
-            end
-            cellName = nodeData.cellName;            
+            cellName = obj.getParameterValue('cellName', nodeInd);
         end
         
         function mode = getMode(obj, nodeInd)
-            mode = '';
-            nodeData = obj.get(nodeInd);
-            while ~isfield(nodeData, 'ampModeParam');
-                 if nodeInd==1
-                    return;
-                end
-                nodeInd = obj.getparent(nodeInd);
-                nodeData = obj.get(nodeInd);
-            end
-            mode = nodeData.(nodeData.ampModeParam);
+            mode = obj.getParameterValue('ampModeParam', nodeInd);
         end
         
         function device = getDevice(obj, nodeInd)
-            nodeData = obj.get(nodeInd);
-            while ~isfield(nodeData, 'deviceName');
-                nodeInd = obj.getparent(nodeInd);
-                nodeData = obj.get(nodeInd);
-            end
-            device = nodeData.deviceName;
+            device = obj.getParameterValue('deviceName', nodeInd);
         end
         
         function className = getClassName(obj, nodeInd)
-            className = [];
+            className = obj.getParameterValue('class', nodeInd);
+        end
+        
+        function value = getParameterValue(obj, parameter, nodeInd)
             nodeData = obj.get(nodeInd);
-            while ~isfield(nodeData, 'class');
-                if nodeInd==1
+            value = [];
+            while ~ isfield(nodeData, parameter);
+                if nodeInd == 0
                     return;
                 end
-                nodeInd = obj.getparent(nodeInd);                
+                
+                nodeInd = obj.getparent(nodeInd);
                 nodeData = obj.get(nodeInd);
             end
-            className = nodeData.class;
+            value = nodeData.(parameter);
         end
         
-        function obj = percolateUp(obj, nodeIDs, varargin)
-            if length(varargin) == 2 && iscell(varargin{1}) && iscell(varargin{2}) %specified as 2 long lists instead
-                inNames = varargin{1};
-                outNames = varargin{2};
-            else %in pairs
-                inNames = varargin(1:2:end);
-                outNames = varargin(2:2:end);
-            end
+        function nodeData = updateNodeDataInStructure(obj, nodeId, in, out)
+            nodeData = obj.get(parent);
+            ref = obj.get(nodeId);
+            nodeData.(out) = [];
+            inStructure = ref.(in);
+            fnames = fieldnames(inStructure);
             
-            if length(inNames) ~= length(outNames)
-                disp('Error: parameters must be specified in pairs');
-            end
-            %                 parentList = [];
-            %                 for i=1:length(nodeIDs)
-            %                     parentList = [parentList obj.getparent(nodeIDs(i))];
-            %                 end
-            %                 parentList = unique(parentList);
-            
-            for p=1:length(inNames)
-                for i=1:length(nodeIDs)
-                    parent = obj.getparent(nodeIDs(i));
-                    %chIndex = find(obj.getchildren(parent) == nodeIDs(i));
-                    nodeData = obj.get(parent);
-                    if isfield(obj.get(nodeIDs(i)), inNames{p})
-                        if ~isfield(nodeData, outNames{p})
-                            nodeData.(outNames{p}) = []; 
-                            %zeros(1,length(obj.getchildren(parent)));
-                        end
-                        %merging struct (special response struct) or just a
-                        %value
-                        if isstruct(obj.get(nodeIDs(i)).(inNames{p}))
-                            S = obj.get(nodeIDs(i)).(inNames{p});
-                            fnames = fieldnames(S);
-                            for f = 1:length(fnames)
-                                curField = fnames{f};
-                                if strcmp(curField, 'units') || strcmp(curField, 'type') 
-                                    %copy once
-                                    nodeData.(outNames{p}).(curField) = S.(curField);
-                                elseif length(S.(curField)) > 1
-                                    %strcmp(curField, 'value') || strcmp(curField, 'value_c') || strcmp(curField, 'outliers')
-                                    %do not copy these
-                                else
-                                    %copy as vector
-                                    if ~isfield(nodeData.(outNames{p}), curField)
-                                        curInd = 1;
-                                    else
-                                        curInd = length(nodeData.(outNames{p}).(curField)) + 1;
-                                    end
-                                    if isempty(S.(curField)) %why am I getting empties here?
-                                        nodeData.(outNames{p}).(curField)(curInd) = NaN; 
-                                    else
-                                        nodeData.(outNames{p}).(curField)(curInd) = S.(curField);
-                                    end
-                                end
-                            end
-                        else
-                            curVec = nodeData.(outNames{p});
-                            curVec = [curVec obj.get(nodeIDs(i)).(inNames{p})];
-                            nodeData.(outNames{p}) = curVec;
-                        end
-                        obj = obj.set(parent, nodeData);
-                    end
+            for i = 1 : length(fnames)
+                field = fnames{i};
+                inValue = inStructure.(field);
+                
+                if strcmp(field, 'units') || stcmp(field, 'type')
+                    nodeData.(out) = inValue;
+                elseif length(inValue) < 2 && isfield(nodeData.(out), field)
+                    % append to end of existing value vector
+                    nodeData.(out).(field)(end + 1) = inValue;
                 end
             end
-            
         end
         
     end
-    
-    methods(Static)
-        
-        function plotLeaf(node, cellData)
-            %do nothing - to be overwritten by each analysis class
-        end
-        
-    end
-    
 end
--- src/main/matlab/+symphony/+analysis/+core/AnalysisTreeBuilder.m
+++ src/main/matlab/+symphony/+analysis/+core/AnalysisTreeBuilder.m
@@ -30,7 +30,7 @@ classdef AnalysisTreeBuilder < handle
                     nodeData.name = [parameter '==' num2str(value)];
                     nodeData.splitValue = value;
                     nodeData.epochID = newDataSet;
-
+                    
                     id = obj.addNode(rootNodeID, nodeData);
                     if length(paramList) > 1
                         obj.buildCellTree(id, cellData, newDataSet, paramList(2 : end));
@@ -54,15 +54,13 @@ classdef AnalysisTreeBuilder < handle
             
             for i = 1 : numel(nodeValueMap.keys)
                 value = nodeValueMap.keys(i);
-                valueIndices = nodeValueMap(value);
+                childIndicesPosition = nodeValueMap(value);
                 
                 obj.addNode(1, struct('name', [param '=' value], param, value));
                 newChildIndices = obj.tree.getchildren(1);
-                newTree = newChildIndices(i);
-                
-                cellfun(@(index) obj.graft(newTree, oldTree.subtree(childIndices(index))), valueIndices)
+                newTreeIndex = newChildIndices(i);
+                obj.graftTreeByChildPositon(childIndicesPosition, childIndices, newTreeIndex, oldTree);
             end
-            
         end
         
         function copyParameters(obj, params)
@@ -77,27 +75,69 @@ classdef AnalysisTreeBuilder < handle
                     obj.tree.(name) = params.(name);
                 end
             end
-            obj.setNode(nodeData);
+            obj.setNode(1, nodeData);
         end
         
-        function percolateUp(obj)
-            % TODO
+        function percolateUp(obj, nodeIds, varargin)
+            
+            if length(varargin) == 2 && iscell(varargin{1}) && iscell(varargin{2})
+                inParams = varargin{1};
+                outParams = varargin{2};
+            else
+                inParams = varargin(1 : 2 : end);
+                outParams = varargin(2 : 2 : end);
+            end
+            
+            if length(inParams) ~= length(outParams)
+                error('Error: parameters must be specified in pairs');
+            end
+            arrayfun(@(i) obj.percolateUpByParameter(nodeIds, inParams{i}, outParams{i}),...
+                1 : length(inParams));
         end
-        
     end
     
     methods(Access = private)
         
-        function setNode(obj, node)
-            obj.tree = obj.tree.set(1, node);
+        function percolateUpByParameter(obj, nodeIds, in , out)
+            t = obj.tree;
+            
+            for i = 1 : length(nodeIds)
+                nodeId = nodeIds(i);
+                parent = t.getparent(nodeId);
+                nodeData = t.get(parent);
+                ref = t.get(nodeId);
+                nodeData.(out) = [];
+                
+                if ~ isfield(ref, in)
+                    continue;
+                end
+                
+                if isstruct(ref.(in))
+                    nodeData = t.updateNodeDataInStructure(nodeId, in, out);
+                else
+                    vector = nodeData.(out);
+                    vector = [vector ref.(in)]; %#ok
+                    nodeData.(out) = vector;
+                end
+                obj.setNode(parent, nodeData);
+            end
+        end
+
+        
+        function setNode(obj, parent, node)
+            obj.tree = obj.tree.set(parent, node);
         end
         
         function addNode(obj, node)
             obj.tree = obj.tree.addnode(1, node);
         end
         
-        function graft(obj, tree1, tree2)
-            obj.tree = obj.tree.graft(tree1, tree2);
+        function graft(obj, index, tree2)
+            obj.tree = obj.tree.graft(index, tree2);
+        end
+        
+        function graftTreeByChildPositon(poistion, childIndices, index, oldTree)
+            cellfun(@(id) obj.graft(index, oldTree.subtree(childIndices(id))), poistion)
         end
     end
 end
