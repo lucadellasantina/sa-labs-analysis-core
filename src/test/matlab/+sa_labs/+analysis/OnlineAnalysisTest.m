adf089144c9965bf8bfda9f08ce570ccd4ca1696
Sathish Kumar Narayanan <ragav.sathish@gmail.com>
2016-12-12 11:47:35 +0200

    update online analysis for few more test cases

--- src/test/matlab/+sa_labs/+analysis/OnlineAnalysisTest.m
+++ src/test/matlab/+sa_labs/+analysis/OnlineAnalysisTest.m
@@ -42,8 +42,8 @@ classdef OnlineAnalysisTest < matlab.unittest.TestCase
                 analysis.setEpochStream(epoch)
                 analysis.service();
             end
-            
             tree = analysis.getResult();
+            expectedNumberOfNodes = tree.nnodes;
             nodes = core.NodeManager(tree).getImmediateChildrensByName('stimTime==500');
             expected = tree.findleaves();
             
@@ -53,6 +53,75 @@ classdef OnlineAnalysisTest < matlab.unittest.TestCase
             obj.verifyEqual(numel(nodes), 2)
             obj.verifyEqual(nodes(1).id, expected(end - 1));
             obj.verifyEqual(nodes(2).id, expected(end));
+            
+            % alternate case - 1 if all the epoch parameter is not present
+            % in analysis template
+            epoch.parameters = containers.Map({'param1', 'param2'}, {300, 500});
+            analysis.setEpochStream(epoch)
+            t = analysis.service();
+            obj.verifyEqual(t.nnodes, expectedNumberOfNodes);
+            
+            % alternate case - 2 if any of the epoch parameter is present
+            % in analysis template
+            epoch.parameters = containers.Map({'stimTime', 'param2'}, {300, 500});
+            analysis.setEpochStream(epoch)
+            t = analysis.service();
+            obj.verifyEqual(t.nnodes, expectedNumberOfNodes);
+        end
+        
+        function testBuildTreeComplex(obj)
+            import sa_labs.analysis.*;
+            
+            % analysis template structure
+            s = struct();
+            s.type = 'complex-analysis';
+            s.extractorClass = 'sa_labs.analysis.core.FeatureExtractor';
+            s.buildTreeBy = {'protocol', 'textureAngle, barAngle, curSpotSize', 'RstarMean'};
+            
+            template = core.AnalysisTemplate(s);
+            analysis = core.OnlineAnalysis();
+            analysis.init(template)
+            
+            
+            parameterKey1 = {'protocol', 'textureAngle', 'RstarMean'};
+            parameterKey2 = {'protocol', 'textureAngle', 'RstarMean'};
+            parameterKey3 = {'protocol', 'barAngle', 'RstarMean'};
+            parameterKey4 = {'protocol', 'curSpotSize', 'RstarMean'};
+            
+            numberOfEpochs = 3;
+            % validate for different RstarMean
+            t = validate(parameterKey1, @(i) {'driftingTexture', 10, i * 10}, 1 + 5);
+            t = validate(parameterKey2, @(i) {'driftingGrating', 30, i * 10}, t.nnodes + 5);
+            t = validate(parameterKey3, @(i) {'movingBar', 90, i * 10}, t.nnodes + 5);
+            
+            logTree();
+            
+            numberOfEpochs = 2;
+            % validate for different angle and size
+            t = validate(parameterKey1, @(i) {'driftingTexture', i * 10, 10}, t.nnodes + 2);
+            t = validate(parameterKey2, @(i) {'driftingGrating', i * 30, 10}, t.nnodes + 2);
+            t = validate(parameterKey3, @(i) {'movingBar', i * 90, 10}, t.nnodes + 2);
+            t = validate(parameterKey4, @(i) {'spotMultiSize', i * 100, 10}, t.nnodes + 5);
+    
+            logTree();
+            
+            function t = validate(k, valueSetHandle, expected)
+                % disp(' [INFO] Awesome Breadth expansion ! ...')
+                for i = 1 : numberOfEpochs
+                    epoch.parameters = containers.Map(k, valueSetHandle(i));
+                    analysis.setEpochStream(epoch)
+                    t = analysis.service();
+                    % logTree()
+                    % pause(1);
+                end
+                obj.verifyEqual(t.nnodes, expected)
+            end
+            
+            function logTree()
+                tree = analysis.getResult();
+                disp('analysis tree')
+                tree.treefun(@(node) node.name).tostring()
+            end
         end
     end
 end
