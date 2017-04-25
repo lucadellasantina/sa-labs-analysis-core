3af176cd436caf5ac66c3b114c45042cf4d3a884
Sathish Kumar Narayanan <ragav.sathish@gmail.com>
2017-04-25 21:43:01 +0300

    get analysis tree by project and experiment

--- src/main/matlab/functional-api/getAnalysisTree.m
+++ src/main/matlab/functional-api/getAnalysisTree.m
@@ -1,11 +1,22 @@
-function analysisTree = getAnalysisTree(experiment)
+function analysisTree = getAnalysisTree(identifier, varargin)
+
+    ip = inputParser();
+    ip.addParameter('isExperiment', false, @islogical);
+    ip.parse(varargin{:});
+    isExperiment = ip.Results.isExperiment;
 
     import sa_labs.analysis.*;
-    dao = getInstance('analysisDao');
-    
-    treeData = dao.findAnalysisResult(dao.findAnalysisResultNames(experiment));
-    analysisTree = core.factory.createFeatureBuilder('project', matlab.lang.makeValidName(char(datetime)),...
-        'data', treeData);
-    analysisTree.getStructure().tostring()
+
+    if ~ isExperiment
+        project  = getAnalysisManager().initializeProject(identifier);
+        analysisTree = core.factory.createFeatureBuilder('project', identifier,...
+            'data', project.getAllresult());
+    else
+        dao = getInstance('analysisDao');
+        treeData = dao.findAnalysisResult(dao.findAnalysisResultNames(experiment));
+        analysisTree = core.factory.createFeatureBuilder('project', matlab.lang.makeValidName(char(datetime)),...
+            'data', treeData);
+    end
+
 end
 
