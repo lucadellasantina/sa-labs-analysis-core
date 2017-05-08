00bb170eb4fac2d5ff36484cc69e05c74c13fded
Sathish Kumar Narayanan <ragav.sathish@gmail.com>
2017-05-08 13:28:47 +0300

    moved find functions to FeatureTreeFinder

--- src/main/matlab/+sa_labs/+analysis/+core/FeatureTreeBuilder.m
+++ src/main/matlab/+sa_labs/+analysis/+core/FeatureTreeBuilder.m
@@ -1,11 +1,6 @@
-classdef FeatureTreeBuilder < handle
+classdef FeatureTreeBuilder < sa_labs.analysis.core.FeatureTreeFinder
     
-    properties(Access = protected)
-        tree
-        log
-    end
-    
-    properties(Dependent)
+    properties (Dependent)
         dataStore
     end
     
@@ -17,9 +12,8 @@ classdef FeatureTreeBuilder < handle
             if nargin < 3
                 dataTree = tree();
             end
-            obj.tree = dataTree;
+            obj@sa_labs.analysis.core.FeatureTreeFinder(dataTree);
             obj.setRootName(name, value);
-            obj.log = logging.getLogger(app.Constants.ANALYSIS_LOGGER);
         end
         
         function setRootName(obj, name, value)
@@ -28,7 +22,7 @@ classdef FeatureTreeBuilder < handle
             featureGroup.id = 1;
             obj.setfeatureGroup(featureGroup.id, featureGroup);
         end
-
+        
         function obj = set.dataStore(obj, dataTree)
             obj.tree = dataTree;
         end
@@ -36,21 +30,21 @@ classdef FeatureTreeBuilder < handle
         function ds = get.dataStore(obj)
             ds = obj.tree;
         end
-
+        
         function append(obj, dataTree, copyEnabled)
-
+            
             if nargin < 3
                 copyEnabled = false;
             end
-
+            
             % This may be a performance hit
             % Think of merging a tree in an alternative way.
-
+            
             obj.tree = obj.tree.graft(1, dataTree);
             childrens = obj.tree.getchildren(1);
             obj.updateDataStoreFeatureGroupId();
             obj.log.info([ dataTree.get(1).name ' is grafted to parant tree '])
-
+            
             if copyEnabled
                 id = childrens(end);
                 group = obj.getFeatureGroups(id);
@@ -59,7 +53,6 @@ classdef FeatureTreeBuilder < handle
                 parent.setParameters(group.parameters);
             end
         end
-
         
         function [id, featureGroup] = addFeatureGroup(obj, id, splitParameter, spiltValue, epochGroup)
             
@@ -117,101 +110,14 @@ classdef FeatureTreeBuilder < handle
             siblings = obj.tree.getsiblings(sourceId);
             ids = siblings(siblings ~= sourceId);
             sourceGroup = obj.getFeatureGroups(sourceId);
-           
+            
             tf = ~ isempty(ids) &&...
                 any(arrayfun(@(id) strcmp(obj.getFeatureGroups(id).name, sourceGroup.name), ids))...
                 && obj.isBasicFeatureGroup(sourceGroup);
         end
-        
-        function tf = isPresent(obj, id)
-            tf = obj.tree.treefun(@(node) node.id == id).any();
-        end
-
-        function tf = isBasicFeatureGroup(obj, featureGroups)
-            tf = ~ isempty(featureGroups) && all(ismember([featureGroups.id], obj.tree.findleaves)) == 1;
-        end
-        
-        function tree = getStructure(obj)
-            tree = obj.tree.treefun(@(featureGroup) strcat(featureGroup.name, ' (' , num2str(featureGroup.id), ') '));
-        end
-        
-        function featureGroups = getFeatureGroups(obj, ids)
-            featureGroups = arrayfun(@(index) obj.tree.get(index), ids, 'UniformOutput', false);
-            featureGroups = [featureGroups{:}];
-        end
-                
-        % TODO move all find functions to visitor
-
-        function featureGroups = find(obj, name, varargin)
-            ip = inputParser;
-            ip.addParameter('has', []);
-            ip.parse(varargin{:});
-            has = ip.Results.has;
-            
-            featureGroups = []; 
-            if isempty(has)
-                featureGroups = obj.findFeatureGroup(name);
-                return;
-            end
-            
-            for i = obj.tree.findpath(has.id, 1)
-                if regexp(obj.tree.get(i).name, ['\w*' name '\w*' ])
-                    featureGroups = obj.tree.get(i);
-                    break;
-                end
-            end
-        end
-        
-        function featureGroups = findFeatureGroup(obj, name)
-            featureGroups = [];
-            
-            if isempty(name)
-                return
-            end
-            indices = find(obj.getStructure().regexp(['\w*' name '\w*']).treefun(@any));
-            featureGroups = obj.getFeatureGroups(indices); %#ok
-        end
-        
-        function id = findFeatureGroupId(obj, name, featureGroupId)
-            
-            if nargin < 3 || isempty(featureGroupId)
-                id = find(obj.getStructure().regexp(['\w*' name '\w*']).treefun(@any));
-                return;
-            end
-            subTree = obj.tree.subtree(featureGroupId);
-            structure = subTree.treefun(@(featureGroup) featureGroup.name);
-            indices = find(structure.regexp(['\w*' name '\w*']).treefun(@any));
-            
-            id = arrayfun(@(index) subTree.get(index).id, indices);
-        end
-        
-        function featureGroups = getAllChildrensByName(obj, regexp)
-            featureGroupsByName = obj.findFeatureGroup(regexp);
-            featureGroups = [];
-            
-            for i = 1 : numel(featureGroupsByName)
-                featureGroup = featureGroupsByName(i);
-                subTree = obj.tree.subtree(featureGroup.id);
-                childFeatureGroups = arrayfun(@(index) subTree.get(index), subTree.depthfirstiterator, 'UniformOutput', false);
-                featureGroups = [featureGroups, childFeatureGroups{:}]; %#ok
-            end
-        end
-        
-        function featureGroups = getImmediateChildrensByName(obj, regexp)
-            featureGroupsByName = obj.findFeatureGroup(regexp);
-            featureGroups = [];
-            
-            for i = 1 : numel(featureGroupsByName)
-                featureGroup = featureGroupsByName(i);
-                childrens = obj.tree.getchildren(featureGroup.id);
-                childFeatureGroups = obj.getFeatureGroups(childrens);
-                featureGroups = [featureGroups, childFeatureGroups]; %#ok
-            end
-        end
     end
     
     methods(Access = private)
-        
         
         function percolateUpFeatureGroup(obj, featureGroupId, in , out)
             t = obj.tree;
---
+++ src/main/matlab/+sa_labs/+analysis/+core/FeatureTreeFinder.m
@@ -0,0 +1,101 @@
+classdef FeatureTreeFinder < handle
+    
+    properties (Access = protected)
+        tree
+        log
+    end
+    
+    methods
+        
+        function obj = FeatureTreeFinder(dataTree)
+            obj.tree = dataTree;
+            obj.log = logging.getLogger(sa_labs.analysis.app.Constants.ANALYSIS_LOGGER);
+        end
+        
+        function tf = isPresent(obj, id)
+            tf = obj.tree.treefun(@(node) node.id == id).any();
+        end
+        
+        function tf = isBasicFeatureGroup(obj, featureGroups)
+            tf = ~ isempty(featureGroups) && all(ismember([featureGroups.id], obj.tree.findleaves)) == 1;
+        end
+        
+        function tree = getStructure(obj)
+            tree = obj.tree.treefun(@(featureGroup) strcat(featureGroup.name, ' (' , num2str(featureGroup.id), ') '));
+        end
+        
+        function featureGroups = getFeatureGroups(obj, ids)
+            featureGroups = arrayfun(@(index) obj.tree.get(index), ids, 'UniformOutput', false);
+            featureGroups = [featureGroups{:}];
+        end
+        
+        function featureGroups = find(obj, name, varargin)
+            ip = inputParser;
+            ip.addParameter('has', []);
+            ip.parse(varargin{:});
+            has = ip.Results.has;
+            
+            featureGroups = [];
+            if isempty(has)
+                featureGroups = obj.findFeatureGroup(name);
+                return;
+            end
+            
+            for i = obj.tree.findpath(has.id, 1)
+                if regexp(obj.tree.get(i).name, ['\w*' name '\w*' ])
+                    featureGroups = obj.tree.get(i);
+                    break;
+                end
+            end
+        end
+        
+        function featureGroups = findFeatureGroup(obj, name)
+            featureGroups = [];
+            
+            if isempty(name)
+                return
+            end
+            indices = find(obj.getStructure().regexp(['\w*' name '\w*']).treefun(@any));
+            featureGroups = obj.getFeatureGroups(indices); %#ok
+        end
+        
+        function id = findFeatureGroupId(obj, name, featureGroupId)
+            
+            if nargin < 3 || isempty(featureGroupId)
+                id = find(obj.getStructure().regexp(['\w*' name '\w*']).treefun(@any));
+                return;
+            end
+            subTree = obj.tree.subtree(featureGroupId);
+            structure = subTree.treefun(@(featureGroup) featureGroup.name);
+            indices = find(structure.regexp(['\w*' name '\w*']).treefun(@any));
+            
+            id = arrayfun(@(index) subTree.get(index).id, indices);
+        end
+        
+        function featureGroups = getAllChildrensByName(obj, regexp)
+            featureGroupsByName = obj.findFeatureGroup(regexp);
+            featureGroups = [];
+            
+            for i = 1 : numel(featureGroupsByName)
+                featureGroup = featureGroupsByName(i);
+                subTree = obj.tree.subtree(featureGroup.id);
+                childFeatureGroups = arrayfun(@(index) subTree.get(index), subTree.depthfirstiterator, 'UniformOutput', false);
+                featureGroups = [featureGroups, childFeatureGroups{:}]; %#ok
+            end
+        end
+        
+        function featureGroups = getImmediateChildrensByName(obj, regexp)
+            featureGroupsByName = obj.findFeatureGroup(regexp);
+            featureGroups = [];
+            
+            for i = 1 : numel(featureGroupsByName)
+                featureGroup = featureGroupsByName(i);
+                childrens = obj.tree.getchildren(featureGroup.id);
+                childFeatureGroups = obj.getFeatureGroups(childrens);
+                featureGroups = [featureGroups, childFeatureGroups]; %#ok
+            end
+        end
+    end
+    
+end
+
