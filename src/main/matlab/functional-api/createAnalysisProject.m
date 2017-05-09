96c59331b1c35372fd12e96fca2bb3507de215be
Sathish Kumar Narayanan <ragav.sathish@gmail.com>
2017-05-09 22:56:47 +0300

    used varargin in functional api

--- src/main/matlab/functional-api/createAnalysisProject.m
+++ src/main/matlab/functional-api/createAnalysisProject.m
@@ -1,33 +1,46 @@
-function [project, offlineAnalysisManager] = createAnalysisProject(projectName, experiments)
+function [project, offlineAnalysisManager] = createAnalysisProject(projectName, varargin)
 
 import sa_labs.analysis.*;
 
-offlineAnalysisManager = getInstance('offlineAnalaysisManager');
+ip = inputParser();
+ip.addParameter('experiments', '', @(e) ischar(e) || iscellstr(e));
+ip.addParameter('user', getenv('username'), @ischar);
+ip.addParameter('description', 'Hope it will be defined later !', @ischar);
 
+ip.parse(varargin{:});
+experiments = ip.Results.experiments;
+user = ip.Results.user;
+description = ip.Results.description;
+
+offlineAnalysisManager = getInstance('offlineAnalaysisManager');
 try
+    
     project = offlineAnalysisManager.initializeProject(projectName);
     
-    if nargin < 2
-        experiments = project.experimentList;
-    end
-    
-    if ~ all(ismember(experiments, project.experimentList))
+    if isNewExperiment(experiments, project.experimentList)
         project.addExperiments(experiments);
         offlineAnalysisManager.createProject(project);
     end
     
 catch exception
+    
     if ~ strcmp(exception.identifier, app.Exceptions.NO_PROJECT.msgId)
-      rethrow(exception);
+        rethrow(exception);
     end
-    disp(exception.message);
     
     fileRepo = getInstance('fileRepository');
+    
     project = entity.AnalysisProject();
     project.identifier = projectName;
     project.analysisDate = fileRepo.dateFormat(date);
     project.addExperiments(experiments);
-    project.performedBy = getenv('username');
+    project.performedBy = user;
+    project.description = description;
+    
     offlineAnalysisManager.createProject(project);
 end
+end
+
+function tf = isNewExperiment(experiments, experimentList)
+tf =  ~ isempty(experiments) && any(~ ismember(experiments, experimentList));
 end
